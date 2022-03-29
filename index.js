5a0bed78a3b055d4d820585f14d7349647106edc
Myoko-1110 <92731952+Myoko-1110@users.noreply.github.com>
2022-03-29 21:11:01 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+client.on ('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+});
+
+client.on ('messageCreate', (message) => {
+    if (message.content === '!ping') {
+        message.reply('pong!')
+    }
+})
+
+
+client.on ('messageCreate', (message) => {
+  if (message.author.bot) return;
+  if (message.content.match('忘れました','わすれました','忘れました')) {
+    message.channel.send('なんでぇ！？')
+  }
+})
+
+client.on ('message', message => {
+  if (message.author.bot) return;
+  if (message.content.match('持ってきます','もってきます','今度','こんど')){
+  message.channel.send('確証が欲しいの確証が！！')
+}
+})
+
+client.on ('message', message => {
+  if (message.author.bot) return;
+  if (message.content.match('いっしょにやろ','一緒にやろ','おつかれ','ビクロイナイス','対あり','たいあり')){
+  message.channel.send('らぁくしょうらぁくしょう！')
+}
+})
+
+client.on ('message', message => {
+  if (message.author.bot) return;
+  if (message.content.match('ねぇねぇ','@everyone','@Myoko','@TSMFTX_tsugumi')){
+    message.channel.send('うるさいんですけどぉーーー')
+}
+})
+
+client.on ('message', message => {
+  if (message.author.bot) return;
+  if (message.isMemberMentioned){
+    message.create('うるさいんですけどぉーーー')
+  }
+})
+
+client.on ('message', message => {
+  if (message.author.bot) return;
+  if (message.content.match('お疲れさまでした','やめる')){
+    message.channel.send('さようなら')
+  }
+})
+
+
+client.on ('message', message => {
+  if (message.author.bot) return;
+  if (message.content.match('@everyone やろ','天谷')){
+  message.channel.send('よろしくおねがいしーth')
+}
+})
+
+
+/* 足りなかったら上のやつコピペして */
+
+
+client.login ("OTU4MzExOTQ3Mzc0MzA5Mzc2.YkLfjA.qbmePTuhN5rAbQxecPv7MPhue4k");
